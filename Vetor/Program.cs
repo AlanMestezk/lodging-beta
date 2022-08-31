489541c8e7cdefb3588739c834f333e4b32d3252
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-08-31 15:16:27 -0300

    Praticando c# - vetores com classe

---
+++ Vetor/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+
+namespace Vetor {
+    class Program {
+        static void Main(string[] args) {
+
+            Vetoh[] reserva = new Vetoh[10];
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("               POUSADA QUE DELICIA ");
+            Console.WriteLine("            O LUGAR ONDE VOCÊ RELAXA ");
+            Console.WriteLine();
+            Console.WriteLine("           BANCO DE RESERVA DE HÓSPEDES");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.Write("          DIGITE A QUANTIDADE DE RESERVAS: ");
+
+            int numero = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= numero; i++) {
+                Console.WriteLine();
+                Console.WriteLine($"  Reserva {i}");
+                Console.Write("        Digite o dia da entrada: ");
+                string dia = Console.ReadLine();
+                Console.Write("        Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("        Email: ");
+                string email = Console.ReadLine();
+                Console.Write($"        Cpf: ");
+                string cpf = Console.ReadLine();
+                Console.Write("        Telefone: ");
+                int telefone = int.Parse(Console.ReadLine());
+                Console.Write("        Numero do quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                reserva[quarto] = new Vetoh(nome, email, cpf, telefone, dia);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("   Quartos ocupados: ");
+            for(int i = 0; i < 10; i++) {
+                if (reserva[i] != null) {
+                    Console.WriteLine();
+                    Console.WriteLine($"      Quarto {i}");
+                    Console.WriteLine($"      {reserva[i]}");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Vetor/Vetoh.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace Vetor {
+    internal class Vetoh {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public int Telefone { get; set; }
+        public string Dia { get; set; }
+
+        public Vetoh(string nome, string email, string cpf, int telefone, string dia) {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Telefone = telefone;
+            Dia = dia;
+        }
+
+        override public string ToString() {
+            Console.WriteLine($"    Hóspede {Nome}");
+            Console.WriteLine($"    Dados => E-mail: {Email} || Cpf: {Cpf} || Telefone: {Telefone}");
+            Console.WriteLine($"    Check in dia: {Dia}");
+            return "";
+
+        }
+    }
+}
